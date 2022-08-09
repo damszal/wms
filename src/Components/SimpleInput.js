201e90633644b71de003db6e6e04374ca1314467
damian <damian.szalimow@onet.eu>
2022-08-09 16:09:48 +0200

    add small validate logic

--- src/Components/SimpleInput.js
+++ src/Components/SimpleInput.js
@@ -3,61 +3,65 @@ import { useState} from 'react';
 
 export default function SimpleInput() {
     const [enteredLogin, setEnteredLogin] = useState('');
-    const [enteredLoginIsValid, setEnteredLoginIsValid] = useState('true');
-    const [enteredPassInput, setEnteredPassInput] = useState('');
-    const [enteredPassInputIsValid, setEnteredPassInputIsValid] = useState('true');
+    const [enteredLoginIsValid, setEnteredLoginIsValid] = useState('false');
+    const [enteredLoginIsTuched, setEnteredLoginIsTuched] = useState('false');
+    // const [enteredPassInput, setEnteredPassInput] = useState('');
+    // const [enteredPassInputIsValid, setEnteredPassInputIsValid] = useState('false');
 
     const loginInputChangeHandler = e => {
         setEnteredLogin(e.target.value)
     }
-    const loginPassChangeHandler = e => {
-        setEnteredPassInput(e.target.value)
+    // const loginPassChangeHandler = e => {
+    //     setEnteredPassInput(e.target.value)
         
-    }
+    // }
 
     const formSubmissionHandler = e => {
+        setEnteredLoginIsTuched(true)
         e.preventDefault();
         if (enteredLogin.trim() === "") {
             setEnteredLoginIsValid(false)
             return
         }
-        if (enteredPassInput.trim() === "") {
-            setEnteredPassInputIsValid(false)
-            return
-        }
+        // if (enteredPassInput.trim() === "") {
+        //     setEnteredPassInputIsValid(false)
+        //     return
+        // }
         setEnteredLoginIsValid(true)
-        setEnteredPassInputIsValid(true)
+        // setEnteredPassInputIsValid(true)
 
         console.log(enteredLogin)
-        console.log(enteredPassInput)
+        // console.log(enteredPassInput)
 
         setEnteredLogin('');
-        setEnteredPassInput('');
+        // setEnteredPassInput('');
     }
-
-    const formIsValid = enteredLogin && enteredPassInput ? 'form-control' : 'form-control invalid'
+    const loginIsInvalid = !enteredLoginIsValid & enteredLoginIsTuched
+    const nameInputClasses = loginIsInvalid 
+    // && enteredPassInput 
+    ? 'form-control invalid' : 'form-control'
     
   return (
     <form onSubmit={formSubmissionHandler}>
-    <div className={formIsValid}>
+    <div className={nameInputClasses}>
       <label htmlFor='login'>Your Login</label>
       <input type='text' 
       id='login' 
       onChange={loginInputChangeHandler}
       value={enteredLogin}
       />
-      <label htmlFor='pass'>Your Password</label>
+      {/* <label htmlFor='pass'>Your Password</label>
       <input type='text' 
       id='pass'
        onChange={loginPassChangeHandler}
         value={enteredPassInput}
-       />
+       /> */}
     </div>
     <div className="form-actions">
       <button>Submit</button>
     </div>
-    {!enteredLoginIsValid && <p className='error-login'>please type something in login field</p>}
-    {!enteredPassInputIsValid && <p className='error-password'>please type something in password field</p>}
+    {loginIsInvalid && <p className='error-login'>please type something in login field</p> || <p></p>}
+    {/* {!enteredPassInputIsValid && <p className='error-password'>please type something in password field</p>} */}
   </form>
   )
 }

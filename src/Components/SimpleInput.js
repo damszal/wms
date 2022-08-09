b878dc317e6aa382361fb68fb581bffa60ddb6d2
damian <damian.szalimow@onet.eu>
2022-08-09 18:49:34 +0200

    refact by shwarz..

--- src/Components/SimpleInput.js
+++ src/Components/SimpleInput.js
@@ -1,67 +1,61 @@
-import React from 'react';
-import { useState} from 'react';
+import { useState } from 'react';
 
-export default function SimpleInput() {
-    const [enteredLogin, setEnteredLogin] = useState('');
-    const [enteredLoginIsValid, setEnteredLoginIsValid] = useState('false');
-    const [enteredLoginIsTuched, setEnteredLoginIsTuched] = useState('false');
-    // const [enteredPassInput, setEnteredPassInput] = useState('');
-    // const [enteredPassInputIsValid, setEnteredPassInputIsValid] = useState('false');
+const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-    const loginInputChangeHandler = e => {
-        setEnteredLogin(e.target.value)
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched; 
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault(); // prevent sending form
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return console.log("no trespassing mr"); // if login is invalid then break the script
     }
-    // const loginPassChangeHandler = e => {
-    //     setEnteredPassInput(e.target.value)
-        
-    // }
 
-    const formSubmissionHandler = e => {
-        setEnteredLoginIsTuched(true)
-        e.preventDefault();
-        if (enteredLogin.trim() === "") {
-            setEnteredLoginIsValid(false)
-            return
-        }
-        // if (enteredPassInput.trim() === "") {
-        //     setEnteredPassInputIsValid(false)
-        //     return
-        // }
-        setEnteredLoginIsValid(true)
-        // setEnteredPassInputIsValid(true)
+    console.log(enteredName);
 
-        console.log(enteredLogin)
-        // console.log(enteredPassInput)
+   
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
 
-        setEnteredLogin('');
-        // setEnteredPassInput('');
-    }
-    const loginIsInvalid = !enteredLoginIsValid & enteredLoginIsTuched
-    const nameInputClasses = loginIsInvalid 
-    // && enteredPassInput 
-    ? 'form-control invalid' : 'form-control'
-    
+  const nameInputClasses = nameInputIsInvalid // if inpot is blank display layout error
+    ? 'form-control invalid'
+    : 'form-control';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-    <div className={nameInputClasses}>
-      <label htmlFor='login'>Your Login</label>
-      <input type='text' 
-      id='login' 
-      onChange={loginInputChangeHandler}
-      value={enteredLogin}
-      />
-      {/* <label htmlFor='pass'>Your Password</label>
-      <input type='text' 
-      id='pass'
-       onChange={loginPassChangeHandler}
-        value={enteredPassInput}
-       /> */}
-    </div>
-    <div className="form-actions">
-      <button>Submit</button>
-    </div>
-    {loginIsInvalid && <p className='error-login'>please type something in login field</p> || <p></p>}
-    {/* {!enteredPassInputIsValid && <p className='error-password'>please type something in password field</p>} */}
-  </form>
-  )
-}
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className='error-text'>Name must not be empty.</p>
+        )} 
+        {/* if inpot is blank display html error */}
+      </div>
+      <div className='form-actions'>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
